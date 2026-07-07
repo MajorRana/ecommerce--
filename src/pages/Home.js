32047fa3a47e22649c6576b162c4fb7c05ee7ec8
Major Singh <webteam1@dfcc.co.in>
2026-07-07 12:28:27 +0530

    fixed home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,26 @@
 'use client'
+import dynamic from 'next/dynamic'
 import Carousel from '@/components/Carousel'
 import CategoryListing from '@/components/CategoryListing'
 import Featured from '@/components/Featured'
 import NavBar from '@/components/NavBar'
 import TopBar from '@/components/TopBar'
 import FeaturedProducts from '@/components/FeaturedProducts'
-import Link from 'next/link'
 import React from 'react'
-import Vender from '@/components/Vender'
 
-function Home() {
+// Humne Home function ka naam badal kar HomeContent kar diya hai
+function HomeContent() {
   return (
     <>
-    <TopBar/>
-    <NavBar/>
-    <Carousel/>
-    <Featured/>
-    <CategoryListing/>
-<FeaturedProducts/>
-{/* <Vender/> */}
+      <TopBar/>
+      <NavBar/>
+      <Carousel/>
+      <Featured/>
+      <CategoryListing/>
+      <FeaturedProducts/>
     </>
   )
 }
 
-export default Home
+// Next.js ko bol rahe hain ki isse sirf browser (client-side) par chalaye, server par nahi
+export default dynamic(() => Promise.resolve(HomeContent), { ssr: false })
